2fb1e49d979397a2fed80cea96196f27c1fa970b
Jon Young <youngt.jonathan>
2025-04-11 13:07:30 -0500

    updates

--- scripts/facilities.js
+++ scripts/facilities.js
@@ -1,8 +1,25 @@
+import { setFacilityId } from "./transientState.js"
+
+const handleFacilityChoice = (changeEvent) => {
+    if (changeEvent.target.id === "facility") {
+        const chosenOption = parseInt(changeEvent.target.value)
+        setFacilityId(chosenOption)
+        console.log(parseInt(chosenOption))
+            }
+         }
+
 export const displayFacilities = async () => {
+    document.addEventListener("change", handleFacilityChoice)
     const response = await fetch("http://localhost:8088/facilities")
     const facilities = await response.json()
+    const selectedGovernorId = document.querySelector("#governor").value;
 
-    let facilityChoiceHTML = `<select id="facility">
+    if (selectedGovernorId === "0") {
+      alert("Please make sure to select a governor");
+      return;
+    }
+
+    let facilityChoiceHTML = `<select id="facility" class="facility-dropdown">
                     <option value="0">Please select facility</option>`
 
     facilities.map((facility) => {
