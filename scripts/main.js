f646834fcd937b84603b0d3ba651d06fd2d070e4
Jon Young <youngt.jonathan>
2025-04-11 15:34:38 -0500

    updated main

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,15 @@
 import { displayGovernors } from "./governors.js";
 import { displayColonyInventory } from "./colonyInventory.js";
 import { governorsEventListener } from "./governors.js";
+import { displayFacilities } from "./facilities.js";
 
 const container = document.querySelector("#container")
 
 const render = async () => {
     const governorsHTML = await displayGovernors()
     const colonyInventoryHTML = await displayColonyInventory()
-    container.innerHTML = governorsHTML + colonyInventoryHTML
+    const facilityChoiceHTML = await displayFacilities()
+    container.innerHTML = governorsHTML + colonyInventoryHTML + facilityChoiceHTML
     governorsEventListener()
 }
 
