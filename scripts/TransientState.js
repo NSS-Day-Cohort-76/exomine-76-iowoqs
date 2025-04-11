dc992cc5814d1f0bf6e20f6f9d828a28b70f7d96
Kyle Mims <kylemims@gmail.com>
2025-04-11 15:16:32 -0500

    aligning modules with the team for debrief

--- scripts/TransientState.js
+++ scripts/TransientState.js
@@ -24,3 +24,62 @@ export const purchaseMineral = () => {
 
     document.dispatchEvent(new CustomEvent("stateChanged"))
 }
+
+
+
+
+
+
+
+
+
+// const transientState = {
+//     governorId: 0,
+//     facilityId: 0,
+//     mineralId: 0
+//   }
+  
+//   // SETTERS
+  
+//   export const setGovernor = (id) => {
+//     transientState.governorId = id
+//     // document.dispatchEvent(new CustomEvent("stateChanged"))
+//   }
+  
+//   export const setFacility = (id => {
+//     transientState.facilityId = id
+//     // document.dispatchEvent(new CustomEvent("stateChanged"))
+//   }
+  
+//   export const setMineral = (id) => {
+//     transientState.mineralId = id
+    // document.dispatchEvent(new CustomEvent("stateChanged"))
+//   }
+
+  // GETTER
+  
+//   export const getTransientState = () => {
+//     return { ...transientState } // safe copy right heeeere
+//   }
+  
+  // RESET
+  
+//   export const resetTransientState = () => {
+//     transientState.mineralId = 0
+//     document.dispatchEvent(new CustomEvent("stateChanged"))
+//   }
+
+
+//main.js  // example of function that keeps on keepin on injecting with each new update
+//   const renderApp = async () => {
+//     document.querySelector("#mineralsForm").innerHTML = FacilityMinerals()
+//   }
+  
+//   document.addEventListener("stateChanged", renderApp)
+
+// FacilityMinerals.js//
+//   document.addEventListener("change", (event) => {
+//     if (event.target.name === "mineral") {
+//       setMineral(parseInt(event.target.value))
+//     }
+//   })
